4b80f6adb75c4d272d5f758ee4c1183518f14f2e
Nicolasre18 <nicolasre18@gmail.com>
2025-06-02 08:25:45 -0500

    Se realizo la logica para obtener y mostrar chistes aleatorios de la API que se nos dio, usando HttpClient para la solicitud y Newtonsoft.Json para procesar la información.

--- Evaluacion2P/Chistes.xaml.cs
+++ Evaluacion2P/Chistes.xaml.cs
@@ -1,14 +1,43 @@
+using Newtonsoft.Json;
+
 namespace Evaluacion2P;
 
 public partial class Chistes : ContentPage
 {
-	public Chistes()
-	{
-		InitializeComponent();
-	}
+    private readonly HttpClient _httpClient = new();
 
-    private void Button_Clicked(object sender, EventArgs e)
+    public Chistes()
     {
+        InitializeComponent();
+        ObtenerChisteAsync();
+    }
 
+    private async void Button_Clicked(object sender, EventArgs e)
+    {
+        await ObtenerChisteAsync();
+    }
+
+    private async Task ObtenerChisteAsync()
+    {
+        try
+        {
+            ChisteLabel.Text = "Aqui va el chiste xd";
+
+            var response = await _httpClient.GetStringAsync("https://official-joke-api.appspot.com/random_joke");
+            var chiste = JsonConvert.DeserializeObject<ChisteModel>(response);
+
+            ChisteLabel.Text = $"{chiste.Setup}\n\n{chiste.Punchline}";
+        }
+        catch (Exception ex)
+        {
+            ChisteLabel.Text = $"Error: {ex.Message}";
+        }
+    }
+
+    public class ChisteModel
+    {
+        public string Type { get; set; }
+        public string Setup { get; set; }
+        public string Punchline { get; set; }
     }
 }
